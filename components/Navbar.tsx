570408dcf7d74af95ae3cf3425c9c709449bd57d
Derryck <oseiagyemanb@gmail.com>
2023-10-05 03:50:28 +0100

    update of file

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { GoogleLogin, GoogleLogout } from '@leecheuk/react-google-login';
-import { AiOutlineLogout } from 'react-icons/ai';
+import { GoogleLogin, googleLogout } from '@react-oauth/google';
+import { HiOutlineLogout } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
 import { IoMdAdd } from 'react-icons/io';
 
 import Logo from '../utils/groovy-logo.png';
+import { createOrGetUser } from '../utils';
+
+import useAuthStore from '../store/authStore';
 
 const Navbar = () => {
+	const [searchValue, setSearchValue] = useState('');
+	const { userProfile, addUser, removeUser } = useAuthStore();
+	const router = useRouter();
+
+	const handleSearch = (e: { preventDefault: () => void }) => {
+		e.preventDefault();
+
+		if (searchValue) {
+			router.push(`/search/${searchValue}`);
+		}
+	};
+
 	return (
 		<div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-4 px-4">
 			<Link href="/">
@@ -22,6 +37,68 @@ const Navbar = () => {
 					/>
 				</div>
 			</Link>
+
+			<div className="relative hidden md:block">
+				<form
+					onSubmit={handleSearch}
+					className="absolute md:static top-10 -left-20 bg-white"
+				>
+					<input
+						type="text"
+						value={searchValue}
+						onChange={(e) => setSearchValue(e.target.value)}
+						placeholder="Search videos and accounts"
+						className="bg-primary border-slate-200 p-3 md:text-md font-medium border-2 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full md:top-0"
+					/>
+					<button
+						onClick={handleSearch}
+						className="absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"
+					>
+						<BiSearch />
+					</button>
+				</form>
+			</div>
+
+			<div>
+				{userProfile ? (
+					<div className="flex gap-5 md:gap-10">
+						<Link href="/upload">
+							<button className="border-2 px-2 md:px-4 text-md font-semibold flex items-center gap-2">
+								<IoMdAdd className="text-xl" />
+								{` `}
+								<span className="hidden md:block">Upload</span>
+							</button>
+						</Link>
+						{userProfile.image && (
+							<Link href="/">
+								<>
+									<Image
+										width={40}
+										height={40}
+										className="rounded-full cursor-pointer"
+										src={userProfile.image}
+										alt="profile photo"
+									/>
+								</>
+							</Link>
+						)}
+						<button
+							type="button"
+							onClick={() => {
+								googleLogout();
+								removeUser();
+							}}
+						>
+							<HiOutlineLogout color="red" fontSize={25} />
+						</button>
+					</div>
+				) : (
+					<GoogleLogin
+						onSuccess={(response) => createOrGetUser(response, addUser)}
+						onError={() => console.log('error')}
+					/>
+				)}
+			</div>
 		</div>
 	);
 };
