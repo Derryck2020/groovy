f94b2e482db59085330c237b012e4854a94e13d4
Derryck <oseiagyemanb@gmail.com>
2023-10-07 05:47:23 +0100

    update of file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,46 @@
+import axios from 'axios';
 import { NextPage } from 'next';
+import { Video } from '../types';
+import VideoCard from '../components/VideoCard';
+import NoResults from '../components/NoResults';
+import { BASE_URL } from '../utils';
 
-const Home: NextPage = () => {
-	return <h1 className="text-5xl font-bold underline">Hello world!</h1>;
+interface IProps {
+	videos: Video[];
+}
+
+const Home = ({ videos }: IProps) => {
+	console.log(videos);
+	return (
+		<div className="flex flex-col gap-10 videos h-full">
+			{videos.length ? (
+				videos.map((video: Video) => (
+					<VideoCard post={video} key={video._id} />
+				))
+			) : (
+				<NoResults text="No Videos" />
+			)}
+		</div>
+	);
+};
+
+export const getServerSideProps = async ({
+	query: { topic },
+}: {
+	query: { topic: string };
+}) => {
+	let response = null;
+	if (topic) {
+		response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+	} else {
+		response = await axios.get(`${BASE_URL}/api/post`);
+	}
+
+	return {
+		props: {
+			videos: response.data,
+		},
+	};
 };
 
 export default Home;
