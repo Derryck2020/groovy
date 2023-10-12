d9055c11f1c11ea7acfe9b2a037424cafc1efb8b
Derryck <oseiagyemanb@gmail.com>
2023-10-12 21:17:38 +0100

    update of file

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { NextPage } from 'next';
+import { MdOutlineVideocamOff } from 'react-icons/md';
+import { BiCommentX } from 'react-icons/bi';
 
 interface Iprops {
 	text: string;
 }
 
 const NoResults: NextPage<Iprops> = ({ text }) => {
-	return <div>NoResults</div>;
+	return (
+		<div className="flex flex-col justify-center items-center h-full w-full">
+			<p className="text-8xl">
+				{text === 'No comments yet! Be first to comment.' ? (
+					<BiCommentX />
+				) : (
+					<MdOutlineVideocamOff />
+				)}
+			</p>
+			<p className="text-center text-[#666666]">{text}</p>
+		</div>
+	);
 };
 
 export default NoResults;
